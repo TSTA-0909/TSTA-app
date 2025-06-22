28c1bc48923151d9e1214cd0efab0a4c729430db
Wataru <tarutarou.prog@gmail.com>
2025-06-22 18:41:57 +0900

    まとめて更新

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,4 @@
 // firebase-messaging-sw.js
-
 importScripts('https://www.gstatic.com/firebasejs/11.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.8.0/firebase-messaging-compat.js');
 
@@ -13,15 +12,3 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-
-// 通知を受け取ったときの処理
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] 背景通知を受信:', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon.png' // 任意のアイコン画像（PWA用と共有でOK）
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
